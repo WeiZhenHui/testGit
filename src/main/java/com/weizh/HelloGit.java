29725621860e734dbca518a7e6a1c4e796455109
WeiZhenHui <18813973732@163.com>
2020-07-01 20:06:24 +0800

    hello git！

---
+++ src/main/java/com/weizh/HelloGit.java
@@ -0,0 +1,12 @@
+package com.weizh;
+/**
+ * @author: weizhenhui
+ * @date: 2020年7月1日, 下午7:53:53
+*/
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git !!!!");
+	}
+
+}
