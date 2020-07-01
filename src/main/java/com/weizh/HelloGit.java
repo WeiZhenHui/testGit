8d8a208034b0a2b7302011b52a6c1349c21ad3ed
WeiZhenHui <18813973732@163.com>
2020-07-01 20:16:33 +0800

    创建分支 dev

--- src/main/java/com/weizh/HelloGit.java
+++ src/main/java/com/weizh/HelloGit.java
@@ -8,6 +8,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git !!!!");
 		System.out.println("update 001");
+		System.out.println("new barch - dev");
+		
 	}
 
 }
