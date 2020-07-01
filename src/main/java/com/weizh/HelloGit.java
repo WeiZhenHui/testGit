48b1d372e0c828563100437ac16ef0a9d8c604de
WeiZhenHui <18813973732@163.com>
2020-07-01 20:46:27 +0800

    master update again

--- src/main/java/com/weizh/HelloGit.java
+++ src/main/java/com/weizh/HelloGit.java
@@ -11,6 +11,7 @@ public class HelloGit {
 		System.out.println("new barch - dev");
 		System.out.println("master update");
 		System.out.println("dev update");
+		System.out.println("master update again");
 	}
 
 }
